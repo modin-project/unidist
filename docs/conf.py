a393665c3e0371092a9d96731f93850c7e42ec2a
Igoshev, Yaroslav <yaroslav.igoshev@intel.com>
2021-11-16 16:02:30 +0300

    DOCS-#7: Add deps for building docs
    Signed-off-by: Igoshev, Yaroslav <yaroslav.igoshev@intel.com>

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,12 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import os
+import sys
+
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 
 # -- Project information -----------------------------------------------------
 
