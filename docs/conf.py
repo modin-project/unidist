5be60f09628afdfd986ad42985ec7649b7907f7b
Yaroslav Igoshev <yaroslav.igoshev@intel.com>
2021-11-17 15:43:06 +0300

    DOCS-#7: Add deps for building docs (#28)
    Signed-off-by: Igoshev, Yaroslav <yaroslav.igoshev@intel.com>

--- docs/conf.py
+++ docs/conf.py
@@ -4,15 +4,11 @@
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Path setup --------------------------------------------------------------
+import os
+import sys
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 
 # -- Project information -----------------------------------------------------
