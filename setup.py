c066ea42ed5ae4f553574c6f8fffa9737beea129
Yaroslav Igoshev <yaroslav.igoshev@intel.com>
2021-11-17 15:13:35 +0300

    FIX-#22: Add dependencies for development and packaging (#23)
    * Add dependencies for development and packaging
    * Apply comments and fix appealing to some packages
    * Correct the license id
    * Exclude pickle5 from env files
    
    Signed-off-by: Igoshev, Yaroslav <yaroslav.igoshev@intel.com>

--- setup.py
+++ setup.py
@@ -1,31 +1,35 @@
-# Always prefer setuptools over distutils
-from setuptools import setup, find_packages
-
 import pathlib
+from setuptools import setup, find_packages
+import sys
 
 here = pathlib.Path(__file__).parent.resolve()
 
 # Get the long description from the README file
 long_description = (here / "README.md").read_text(encoding="utf-8")
 
+ray_deps = ["ray[default]", "pandas"]
+dask_deps = ["dask[complete]>=2.22.0", "distributed>=2.22.0"]
+mpi_deps = ["pandas", "msgpack"]
+if sys.version_info[1] < 8:
+    mpi_deps += "pickle5"
+all_deps = ray_deps + dask_deps + mpi_deps
+
 setup(
     name="unidist",
     version="0.0.1",
-    description="Unified Distributed Execution",  # Optional
-    long_description=long_description,  # Optional
-    long_description_content_type="text/markdown",  # Optional (see note above)
-    author="unidist team",  # Optional
-    author_email="author@example.com",  # Optional
-    # Classifiers help users find your project by categorizing it.
-    # For a list of valid classifiers, see https://pypi.org/classifiers/
-    classifiers=[  # Optional
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        "Development Status :: 3 - Alpha",
-        "Programming Language :: Python :: 3",
-    ],
-    packages=find_packages(),  # Required
-    python_requires=">=3.6",
+    description="Unified Distributed Execution",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="Apache-2.0",
+    packages=find_packages(),
+    url="https://github.com/modin-project/unidist",
+    install_requires=["packaging", "cloudpickle"],
+    extras_require={
+        # can be installed by pip install unidist[ray]
+        "ray": ray_deps,
+        "dask": dask_deps,
+        "mpi": mpi_deps,
+        "all": all_deps,
+    },
+    python_requires=">=3.7.1",
 )
