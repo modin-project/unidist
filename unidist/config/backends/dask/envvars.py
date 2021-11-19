64167f8f4b589240af755e0b1ef52784377f1c21
Alexey Prutskov <alexey.prutskov@intel.com>
2021-11-19 10:51:17 +0300

    FEAT-#38: Add configs for Dask cluster setup (#39)
    Signed-off-by: Alexey Prutskov <alexey.prutskov@intel.com>

--- unidist/config/backends/dask/envvars.py
+++ unidist/config/backends/dask/envvars.py
@@ -4,10 +4,22 @@
 
 """Config entities specific for Dask backend which can be used for unidist behavior tuning."""
 
-from unidist.config.parameter import EnvironmentVariable
+from unidist.config.parameter import EnvironmentVariable, ExactStr
 
 
 class DaskMemoryLimit(EnvironmentVariable, type=int):
     """How many bytes of memory that Dask worker should use."""
 
     varname = "UNIDIST_DASK_MEMORY_LIMIT"
+
+
+class IsDaskCluster(EnvironmentVariable, type=bool):
+    """Whether Dask is running on pre-initialized Dask cluster."""
+
+    varname = "UNIDIST_DASK_CLUSTER"
+
+
+class DaskSchedulerAddress(EnvironmentVariable, type=ExactStr):
+    """Dask Scheduler address to connect to when running in Dask cluster."""
+
+    varname = "UNIDIST_DASK_SCHEDULER_ADDRESS"
