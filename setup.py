a1061d580ea8f0791436a8ac645906741fcf8225
Alexey Prutskov <alexey.prutskov@intel.com>
2021-11-23 17:18:35 +0300

    Add missed change for setup.py

--- setup.py
+++ setup.py
@@ -31,6 +31,6 @@ setup(
         "mpi": mpi_deps,
         "all": all_deps,
     },
-    entry_points={"console_scripts": ["unidist = unidist.cli.run:main"]},
+    entry_points={"console_scripts": ["unidist = unidist.cli.__main__:main"]},
     python_requires=">=3.7.1",
 )
