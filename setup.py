2dba1a7142d821075e073e5cff6d8369facad1d1
Yaroslav Igoshev <yaroslav.igoshev@intel.com>
2022-02-01 00:05:35 +0300

    FIX-#118: Choose dependencies for `pip install unidist[all]` based on platform (#119)
    Signed-off-by: Igoshev, Yaroslav <yaroslav.igoshev@intel.com>

--- setup.py
+++ setup.py
@@ -1,22 +1,67 @@
+import os
 import pathlib
 from setuptools import setup, find_packages
+from setuptools.dist import Distribution
+import sys
 import versioneer
 
+try:
+    from wheel.bdist_wheel import bdist_wheel
+
+    HAS_WHEEL = True
+except ImportError:
+    HAS_WHEEL = False
+
+if HAS_WHEEL:
+
+    class UnidistWheel(bdist_wheel):
+        def finalize_options(self):
+            bdist_wheel.finalize_options(self)
+            self.root_is_pure = False
+
+        def get_tag(self):
+            _, _, plat = bdist_wheel.get_tag(self)
+            py = "py3"
+            abi = "none"
+            return py, abi, plat
+
+
+class UnidistDistribution(Distribution):
+    def __init__(self, *attrs):
+        Distribution.__init__(self, *attrs)
+        if HAS_WHEEL:
+            self.cmdclass["bdist_wheel"] = UnidistWheel
+
+    def is_pure(self):
+        return False
+
+
+ray_deps = ["ray[default]"]
+dask_deps = ["dask[complete]>=2.22.0", "distributed>=2.22.0"]
+mpi_deps = ["mpi4py-mpich", "msgpack"]
+
+if "SETUP_PLAT_NAME" in os.environ:
+    if "macos" in os.environ["SETUP_PLAT_NAME"]:
+        all_deps = ray_deps + dask_deps
+    else:
+        all_deps = ray_deps + dask_deps + mpi_deps
+else:
+    all_deps = (
+        ray_deps + dask_deps
+        if sys.platform == "darwin"
+        else dask_deps + ray_deps + mpi_deps
+    )
 
 here = pathlib.Path(__file__).parent.resolve()
 
 # Get the long description from the README file
 long_description = (here / "README.md").read_text(encoding="utf-8")
 
-ray_deps = ["ray[default]"]
-dask_deps = ["dask[complete]>=2.22.0", "distributed>=2.22.0"]
-mpi_deps = ["mpi4py-mpich", "msgpack"]
-all_deps = ray_deps + dask_deps + mpi_deps
-
 setup(
     name="unidist",
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
+    distclass=UnidistDistribution,
     description="Unified Distributed Execution",
     long_description=long_description,
     long_description_content_type="text/markdown",
