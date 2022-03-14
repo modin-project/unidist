a6139352289f92f7f51bdb8238ccdefd8be10496
Alexey Prutskov <alexey.prutskov@intel.com>
2022-03-14 19:07:29 +0300

    FIX-#69: Pin min msgpack version
    Signed-off-by: Alexey Prutskov <alexey.prutskov@intel.com>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import versioneer
 
 ray_deps = ["ray[default]"]
 dask_deps = ["dask[complete]>=2.22.0", "distributed>=2.22.0"]
-mpi_deps = ["mpi4py-mpich", "msgpack"]
+mpi_deps = ["mpi4py-mpich", "msgpack>=1.0.0"]
 if sys.version_info[1] < 8:
     mpi_deps += "pickle5"
 all_deps = ray_deps + dask_deps + mpi_deps
