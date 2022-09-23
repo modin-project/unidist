30227cb9767a825483be7ed000baafb37568fde9
Igoshev <yigoshev@yigoshev-mobl3.ger.corp.intel.com>
2022-09-23 17:21:28 +0200

    REFACTOR-#149: Cut off unidist CLI
    Signed-off-by: Igoshev, Iaroslav <iaroslav.igoshev@intel.com>

--- setup.py
+++ setup.py
@@ -33,6 +33,5 @@ setup(
         "mpi": mpi_deps,
         "all": all_deps,
     },
-    entry_points={"console_scripts": ["unidist = unidist.cli.__main__:main"]},
     python_requires=">=3.7.1",
 )
