e963e3cf87fa8ab1ec619917dca76c0f944dcf0b
arunjose696 <arunjose696@gmail.com>
2023-03-09 09:44:41 -0600

    checking if gc is present

--- unidist/core/backends/mpi/core/common.py
+++ unidist/core/backends/mpi/core/common.py
@@ -118,7 +118,8 @@ class MasterDataID(DataID):
     def __getstate__(self):
         """Remove a reference to garbage collector for correct `pickle` serialization."""
         attributes = self.__dict__.copy()
-        del attributes["_gc"]
+        if hasattr(self, "_gc"):
+            del attributes["_gc"]
         return attributes
 
     def base_data_id(self):
