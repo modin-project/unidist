edbaadf80d5911d3723268950a42b776282448dc
Arun Jose <40291569+arunjose696@users.noreply.github.com>
2023-04-24 11:15:19 +0200

    FIX-#283: Exit monitor and worker processes in SPMD mode (#284)
    Co-authored-by: Iaroslav Igoshev <Poolliver868@mail.ru>
    Signed-off-by: arunjose696 <arunjose696@gmail.com>

--- unidist/core/backends/mpi/core/controller/api.py
+++ unidist/core/backends/mpi/core/controller/api.py
@@ -200,6 +200,11 @@ def init():
         from unidist.core.backends.mpi.core.monitor import monitor_loop
 
         monitor_loop()
+        # If the user executes a program in SPMD mode,
+        # we do not want workers to continue the flow after `unidist.init()`
+        # so just killing them.
+        if not IsMpiSpawnWorkers.get():
+            sys.exit()
         return
 
     if mpi_state.rank not in (
@@ -209,6 +214,11 @@ def init():
         from unidist.core.backends.mpi.core.worker.loop import worker_loop
 
         asyncio.run(worker_loop())
+        # If the user executes a program in SPMD mode,
+        # we do not want workers to continue the flow after `unidist.init()`
+        # so just killing them.
+        if not IsMpiSpawnWorkers.get():
+            sys.exit()
         return
 
 
