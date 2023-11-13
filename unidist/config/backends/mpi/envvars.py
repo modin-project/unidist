7cf39031b2be4e9b2c94e307cde8656e8d18b43a
Iaroslav Igoshev <iaroslav.igoshev@intel.com>
2023-11-13 11:41:45 +0100

    FIX-#000: Revert PR #387 (#388)
    Signed-off-by: Igoshev, Iaroslav <iaroslav.igoshev@intel.com>

--- unidist/config/backends/mpi/envvars.py
+++ unidist/config/backends/mpi/envvars.py
@@ -87,7 +87,7 @@ class MpiLog(EnvironmentVariable, type=bool):
 class MpiSharedObjectStore(EnvironmentVariable, type=bool):
     """Whether to enable shared object store or not."""
 
-    default = True
+    default = False
     varname = "UNIDIST_MPI_SHARED_OBJECT_STORE"
 
 
