f103df3da0f0eb72b18564dfeec543cae6a8c1af
Iaroslav Igoshev <iaroslav.igoshev@intel.com>
2024-05-28 10:25:24 +0200

    FIX-#416: Modify setup.py to enable python and platforms specific builds (#417)
    Signed-off-by: Igoshev, Iaroslav <iaroslav.igoshev@intel.com>

--- setup.py
+++ setup.py
@@ -1,40 +1,8 @@
 import pathlib
 from setuptools import setup, find_packages, Extension
-from setuptools.dist import Distribution
 from Cython.Build import cythonize
 import sys
 import versioneer
-
-
-try:
-    from wheel.bdist_wheel import bdist_wheel
-
-    HAS_WHEEL = True
-except ImportError:
-    HAS_WHEEL = False
-
-if HAS_WHEEL:
-
-    class UnidistWheel(bdist_wheel):
-        def finalize_options(self):
-            bdist_wheel.finalize_options(self)
-            self.root_is_pure = False
-
-        def get_tag(self):
-            _, _, plat = bdist_wheel.get_tag(self)
-            py = "py3"
-            abi = "none"
-            return py, abi, plat
-
-
-class UnidistDistribution(Distribution):
-    def __init__(self, *attrs):
-        Distribution.__init__(self, *attrs)
-        if HAS_WHEEL:
-            self.cmdclass["bdist_wheel"] = UnidistWheel
-
-    def is_pure(self):
-        return False
 
 
 # https://github.com/modin-project/unidist/issues/324
@@ -60,7 +28,6 @@ setup(
     name="unidist",
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
-    distclass=UnidistDistribution,
     description="Unified Distributed Execution",
     long_description=long_description,
     long_description_content_type="text/markdown",
